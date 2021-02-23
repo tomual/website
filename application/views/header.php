917aa134bfd110c3e66309617a1c9781014c2247
tomual <tomual@users.noreply.github.com>
2021-02-23 21:44:20 +0800

    Complete first version - Header, fonts

--- application/views/header.php
+++ application/views/header.php
@@ -2,166 +2,182 @@
 <html>
 <head>
     <title><?php echo get_title() ? get_title() . ' | ' : null ?>Tomual</title>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-89678350-1');
+    </script>
+    
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap');
+
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: lucida grande,verdana,arial,helvetica;
+            font-size: 12px;
+        }
+        .outer {
+            width: 1000px;
+            margin: auto;
+            position: relative;
+        }
+        .header {
+            background: #666;
+            width: 100%;
+            height: 160px;
+            border: 1px solid black;
+            background-image: url(<?php echo base_url('img/banner_bg.jpg') ?>);
+            background-size: cover;
+        }
+        .white {
+            background: #FFF;
+            border: 1px solid black;
+            padding: 24px;
+            min-height: calc(100vh - 280px);
+        }
+        .nav-item {
+            display: inline-block;
+            border: 1px solid #000;
+            position: relative;
+            padding: 12px 24px;
+            bottom: -1px;
+        }
+        .nav-item.active {
+            border-bottom: 1px solid #FFF;
+        }
+        .content {
+            width: 680px;
+            float: left;
+        }
+        .side {
+            width: 250px;
+            float: right;
+        }
+        .clear {
+            clear: both;
+        }
+        .side-box {
+            background: #EEE;
+            padding: 12px;
+            height: 200px;
+        }
+        textarea.code {
+            background: #000;
+            color: #FFF;
+            font-family: monospace;
+            padding: 12px;
+            width: 100%;
+        }
+        .breadcrumbs {
+            background: #EEE;
+            display: inline-block;
+            padding: 3px 6px;
+            font-size: 12px;
+            text-transform: uppercase;
+            letter-spacing: 0.5px;
+            color: #666;
+        }
+
+        .breadcrumbs a {
+            text-decoration: none;
+            color: #666;
+            font-weight: bold;
+        }
+
+        .nav a {
+            color: #000;
+            text-decoration: none;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            font-size: 11px;
+            font-weight: bold;
+        }
+        h1 {
+            background: #000;
+            color: #FFF;
+            font-size: 14px;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            padding: 3px 6px;
+        }
+
+        h2 {
+            font-size: 1em;
+            border-bottom: 1px dotted #000;
+        }
+        .border {
+            border: 1px solid #000;
+        }
+
+        /* SOFTWARE*/
+        .thumbnail > div {
+            height: 85px;
+            width: 145px;
+            border: 1px dashed #000;
+            vertical-align: middle;
+            background-size: 160%;
+            background-position: top;
+            float: left;
+            margin-right: 14px;
+        }
+
+        .item {
+            margin-bottom: 24px;
+            min-height: 110px;
+        }
+
+        .item-small {
+            margin-bottom: 24px;
+        }
+        h2 a {
+            text-decoration: none;
+            color: #000;
+        }
+        .post-info {
+            font-style: italic;
+            margin-bottom: 12px;
+        }
+        .nav-item.nav-top {
+            border: none;
+        }
+        .site-title {
+            color: #FFF;
+            margin-top: 40px;
+            margin-left: 70px;
+        }
+        .site-title a {
+            color: #FFF;
+            text-decoration: none;
+            font-family: 'Nanum Pen Script', cursive;
+            font-size: 45px;
+        }
+        .update {
+            display: inline;
+            margin-right: 24px;
+        }
+        .updates {
+            font-size: 11px;
+            margin-top: 6px;
+            margin-bottom: 3px;
+        }
+        .yasu {
+            position: absolute;
+            width: 360px;
+            right: 20px;
+            top: 57px;
+        }
+    </style>
 </head>
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-        font-family: lucida grande,verdana,arial,helvetica;
-        font-size: 12px;
-    }
-    .outer {
-        width: 1000px;
-        margin: auto;
-    }
-    .header {
-        background: #666;
-        width: 100%;
-        height: 160px;
-        border: 1px solid black;
-        background-image: url(<?php echo base_url('img/bannerino.png') ?>);
-        background-size: cover;
-    }
-    .white {
-        background: #FFF;
-        border: 1px solid black;
-        padding: 24px;
-        min-height: calc(100vh - 280px);
-    }
-    .nav-item {
-        display: inline-block;
-        border: 1px solid #000;
-        position: relative;
-        padding: 12px 24px;
-        bottom: -1px;
-    }
-    .nav-item.active {
-        border-bottom: 1px solid #FFF;
-    }
-    .content {
-        width: 680px;
-        float: left;
-    }
-    .side {
-        width: 250px;
-        float: right;
-    }
-    .clear {
-        clear: both;
-    }
-    .side-box {
-        background: #EEE;
-        padding: 12px;
-        height: 200px;
-    }
-    textarea.code {
-        background: #000;
-        color: #FFF;
-        font-family: monospace;
-        padding: 12px;
-        width: 100%;
-    }
-    .breadcrumbs {
-        background: #EEE;
-        display: inline-block;
-        padding: 3px 6px;
-        font-size: 12px;
-        text-transform: uppercase;
-        letter-spacing: 0.5px;
-        color: #666;
-    }
-
-    .breadcrumbs a {
-        text-decoration: none;
-        color: #666;
-        font-weight: bold;
-    }
-
-    .nav a {
-        color: #000;
-        text-decoration: none;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        font-size: 11px;
-        font-weight: bold;
-    }
-    h1 {
-        background: #000;
-        color: #FFF;
-        font-size: 14px;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        padding: 3px 6px;
-    }
-
-    h2 {
-        font-size: 1em;
-        border-bottom: 1px dotted #000;
-    }
-    .border {
-        border: 1px solid #000;
-    }
-
-    /* SOFTWARE*/
-    .thumbnail > div {
-        height: 85px;
-        width: 145px;
-        border: 1px dashed #000;
-        vertical-align: middle;
-        background-size: 160%;
-        background-position: top;
-        float: left;
-        margin-right: 14px;
-    }
-
-    .item {
-        margin-bottom: 24px;
-        min-height: 110px;
-    }
-
-    .item-small {
-        margin-bottom: 24px;
-    }
-    h2 a {
-        text-decoration: none;
-        color: #000;
-    }
-    .post-info {
-        font-style: italic;
-        margin-bottom: 12px;
-    }
-    .nav-item.nav-top {
-        border: none;
-    }
-    .site-title {
-        background: #FFF;
-        display: inline-block;
-        position: relative;
-        top: -1px;
-        left: -1px;
-        padding: 12px 24px;
-        font-size: 18px;
-        font-weight: bold;
-        letter-spacing: 1px;
-    }
-    .site-title a {
-        color: #000;
-        text-decoration: none;
-    }
-    .update {
-        display: inline;
-        margin-right: 24px;
-    }
-    .updates {
-        font-size: 11px;
-        margin-top: 6px;
-        margin-bottom: 3px;
-    }
-</style>
 <body>
     <div class="outer">
         <div class="header">
-            <div class="site-title"><a href="<?php echo base_url() ?>">Tomual</a></div>
+            <div class="site-title">
+                <a href="<?php echo base_url() ?>">Tomual</a>
+                <div class="subtitle">Personal site and laboratory</div>
+            </div>
         </div>
 
         <marquee class="updates">
@@ -176,6 +192,7 @@
             <a class="nav-item <?php echo $this->router->fetch_class() == 'about' ? 'active' : null ?>" href="<?php echo base_url('about') ?>">about</a>
             <a class="nav-item nav-top <?php echo $this->router->fetch_class() == 'about' ? 'active' : null ?>" href="<?php echo base_url() ?>">top</a>
         </div>
+        <img class="yasu" src="<?php echo base_url('img/yasu.png') ?>">
         <div class="white">
             <div class="main">
                 <div class="content">
--- application/views/home.php
+++ application/views/home.php
@@ -2,90 +2,112 @@
 <html>
 <head>
     <title>Tomual</title>
-</head>
-<style>
-    body {
-        font-family: lucida grande,verdana,arial,helvetica;
-        font-size: 12px;
-    }
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89678350-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-89678350-1');
+    </script>
     
-    .container {
-        width: 400px;
-        height: 500px;
-        margin: auto;
-        margin-top: 10%;
-        position: relative;
-    }
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap');
+        body {
+            font-family: lucida grande,verdana,arial,helvetica;
+            font-size: 12px;
+        }
+        
+        .container {
+            width: 400px;
+            height: 500px;
+            margin: auto;
+            margin-top: 170px;
+            position: relative;
+        }
 
-    h1 {
-        margin: 0;
-        padding: 0;
-    }
+        h1 {
+            font-family: 'Nanum Pen Script', cursive;
+            margin: 0;
+            padding: 0;
+            font-size: 46px;
+            font-weight: normal;
+            line-height: 42px;
+        }
 
-    .subtitle {
-        font-size: 12px;
-    }
+        .subtitle {
+            font-size: 12px;
+        }
 
-    .white {
-        border: 1px solid #000;
-        padding: 12px;
-        margin: 6px 0;
-    }
+        .white {
+            border: 1px solid #000;
+            padding: 12px;
+            margin: 6px 0;
+        }
 
-    .banner {
-        height: 100px;
-        border: 1px solid black;
-        background: #666;
-        text-align: end;
-        padding: 12px;
-    }
+        .banner {
+            height: 100px;
+            border: 1px solid black;
+            background: #666;
+            text-align: end;
+            padding: 12px;
+            background-image: url(<?php echo base_url('img/front_bg.jpg') ?>);
+            background-position: 1px;
+            background-size: cover;
+            color: #EEE;
+            font-size: 10px;
 
-    .nav a {
-        display: inline-block;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        font-weight: bold;
-        color: black;
-        text-decoration: none;
-        margin-top: 12px;
-        border-bottom: 1px dotted black;
-    }
+        }
 
-    .updates {
-        width: 340px;
-        margin-left: 60px;
-        overflow: hidden;
-        height: 14px;
-    }
+        .nav a {
+            display: inline-block;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+            margin-top: 12px;
+            border-bottom: 1px dotted black;
+        }
 
-    .update {
-        display: inline;
-        margin-right: 24px;
-    }
+        .updates {
+            width: 340px;
+            margin-left: 60px;
+            overflow: hidden;
+            height: 14px;
+        }
 
-    .updates {
-        /* margin-top: 6px; */
-    }
+        .update {
+            display: inline;
+            margin-right: 24px;
+        }
 
-    .item-desc {
-        font-size: 12px;
-    }
+        .updates {
+            /* margin-top: 6px; */
+        }
 
-    img {
-        position: absolute;
-        height: 670px;
-        top: -130px;
-        pointer-events: none;
-        left: -145px;
-    }
+        .item-desc {
+            font-size: 12px;
+        }
 
-    .title {
-        margin-left: 110px;
-    }
-</style>
+        img {
+            position: absolute;
+            height: 670px;
+            top: -130px;
+            pointer-events: none;
+            left: -145px;
+        }
+
+        .title {
+            margin-left: 110px;
+        }
+    </style>
+
+</head>
 <body>
     <div class="container">
-        <img src="<?php echo base_url('img/cattom.png') ?>">
+        <img src="<?php echo base_url('img/yasu_front.png') ?>">
         <div class="title">
             <h1>Tomual</h1>
             <div class="subtitle">Personal site and laboratory</div>
